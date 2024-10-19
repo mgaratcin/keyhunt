bf46dee0cc2e80e294a8b1e448f381934321b230
mgaratcin <159758100+mgaratcin@users.noreply.github.com>
2024-10-19 16:13:49 -0600

    Update kangaroo.h

--- kangaroo.h
+++ kangaroo.h
@@ -1,6 +1,15 @@
 #ifndef KANGAROO_H
 #define KANGAROO_H
 
-void kangaroo_algorithm();
+#include <stdint.h>
+
+// Struct to represent a kangaroo position in the search
+typedef struct {
+    uint64_t position;
+    uint64_t value;
+} Kangaroo;
+
+// Function to run the Pollard's kangaroo algorithm
+void kangaroo_algorithm(uint64_t start, uint64_t end, uint64_t target);
 
 #endif // KANGAROO_H
