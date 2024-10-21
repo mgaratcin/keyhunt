c922ea2bf8e93b96c9f05a7984def2204502a805
mgaratcin <159758100+mgaratcin@users.noreply.github.com>
2024-10-20 23:43:41 -0600

    Create deploy_kangaroos.h

---
+++ deploy_kangaroos.h
@@ -0,0 +1,11 @@
+// deploy_kangaroos.h
+#ifndef DEPLOY_KANGAROOS_H
+#define DEPLOY_KANGAROOS_H
+
+#include <vector>
+#include "secp256k1/Int.h"
+
+// Function declaration for deploying kangaroos
+void deploy_kangaroos(const std::vector<Int>& kangaroo_batch);
+
+#endif // DEPLOY_KANGAROOS_H
